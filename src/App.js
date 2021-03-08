c33938e127768a07019e1ae8a2f407f8787d66d8
Dewey C <deweycraddock@gmail.com>
2021-03-07 19:23:36 -0600

    Recipe Component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
+import Recipes from './Components/RecipesComponent';
 import './App.css';
 
-function App() {
+
+const App = () => {
+
+  // GETTING API INFO FOR RECIPES 
+  const APP_ID = '62230376';
+  const APP_KEY = 'bb36b2ba882060db9cc10410d5fba259';
+  // GET REQUEST
+  const API_REQ = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  // Set State
+  const [recipes, setRecipes] = useState([]);
+
+  //use effect is a function that runs when a num of times in this case on load it will run once (, []) = only run once
+  useEffect(() => {
+    // outside async function to Request recipes
+    getRecipes();
+
+  }, []);
+
+
+  // ASYNC await function to get recipes 
+  const getRecipes = async () => {
+    // wait to fetch api request and stor in var response
+    const response = await fetch(API_REQ);
+    // wait for a response and use method to make json and store as date
+    const data = await response.json();
+    // set state of recipes to [] of recipes from request
+    setRecipes(data.hits)
+
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form className='search-form'>
+        <input className='search-bar' type="text" />
+        <button className='search-btn' type='submit'>
+          Search
+        </button>
+
+      </form>
+      {
+        //mapping through recipes from state  and returning each recipe / returns component for each recipe
+      }
+      {recipes.map(recipe => (
+        <Recipes />
+      ))};
     </div>
+
   );
 }
 
 export default App;
+
+
+
+
+  // todo  look up useEffect 
+  // todo  look up async await  
+  // todo  look up hooks 
+
---
+++ src/Components/RecipesComponent.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const RecipesComponent = () => {
+    return (
+        <div style={{ border: 'solid black 1px' }}>
+            <img src="#" alt="#" />
+            <h1>Title</h1>
+            <p className="calories">Cals</p>
+            <p className="diet-labels">Low Carb</p>
+            <p className="ingredients">Chicken</p>
+        </div>
+    )
+}
+
+export default RecipesComponent
